66d65ba92c46a277d2920926313751ff651983a8
Andrii Sydorenko <andriisydorenko@ylbvconsulting.co.uk>
2026-04-15 17:17:25 +0100

    Add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, DM_Serif_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const dmSerif = DM_Serif_Display({
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} ${dmSerif.variable}`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
