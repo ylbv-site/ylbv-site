e0e4102938d204cd4ac7bcc34f36d8573fe28e07
Andrii Sydorenko <andriisydorenko@ylbvconsulting.co.uk>
2026-04-24 17:58:29 +0100

    Add Google Analytics G-XS5NQDPHNQ

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GA_ID = "G-XS5NQDPHNQ";
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
